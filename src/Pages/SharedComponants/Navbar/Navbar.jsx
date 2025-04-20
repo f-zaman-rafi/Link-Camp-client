9f64816c2cbadd2f017432720f4322e1d55c8ef1
f-zaman-rafi <f.zaman.rafi@gmail.com>
2025-04-21 00:31:47 +0800

    initial navbar done

--- src/Pages/SharedComponants/Navbar/Navbar.jsx
+++ src/Pages/SharedComponants/Navbar/Navbar.jsx
@@ -4,14 +4,23 @@ import { MdAnnouncement } from "react-icons/md";
 import { FaEdit } from "react-icons/fa";
 import { IoPersonCircleSharp } from "react-icons/io5";
 import { FaUniversity } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="">
+    <div className="flex items-center px-5">
       <div>
-        <FaUniversity />
+        <Link
+          to="/"
+          className="flex flex-col text-center items-center text-red-600"
+        >
+          <FaUniversity className="border-2 border-red-600 rounded-full p-[1px] text-2xl" />
+          <p className="font-medium">LinkCamp</p>
+        </Link>
       </div>
+
       {/* name of each tab group should be unique */}
+
       <div className="tabs flex justify-center w-full py-5">
         <label className="tab flex-1">
           <input
@@ -22,21 +31,19 @@ const Navbar = () => {
           />
           <HiHome className="text-4xl peer-checked:text-red-600" />
         </label>
-        <div className="tab-content text-center py-10">Tab content 1</div>
+
         <label className="tab flex-1">
           <input type="radio" name="tabs" className="peer hidden" />
           <MdAnnouncement className="text-4xl peer-checked:text-red-600" />
         </label>
-        <div className="tab-content text-center py-10">Tab content 2</div>
         <label className="tab flex-1">
           <input type="radio" name="tabs" className="peer hidden" />
           <FaEdit className="text-4xl peer-checked:text-red-600" />
         </label>
-        <div className="tab-content text-center py-10">Tab content 3</div>
       </div>
-      <div>
+      <Link to="/" className="text-4xl">
         <IoPersonCircleSharp />
-      </div>
+      </Link>
     </div>
   );
 };
